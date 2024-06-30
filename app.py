6966cd821259d32a8186edd08c88bad864ed309e
Monae <93678860+purplemo340@users.noreply.github.com>
2024-06-30 19:15:35 -0400

    Updated css file and fixed button links

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 class Base(DeclarativeBase):
   pass
 db=SQLAlchemy(model_class=Base)
